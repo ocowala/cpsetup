3b3af5015e8670ce58b634b56e76e7c173cb5a23
Advitiya Jadhav <86247407+ocowala@users.noreply.github.com>
2021-11-28 23:06:47 -0500

    Add files via upload
    main.cpp looks weird because of nvim coc-clangd, try to resolve using ycm-core

--- download_prob.py
+++ download_prob.py
@@ -18,46 +18,83 @@ def get_io_type():
         print(i)
     for i in a_json['output']:
         print(i)
+def make_samples(dir):
+    os.system(f"mkdir {dir}/samples")
+    os.chdir("/Users/sandisk/Desktop/tmp/programming/cpp2")
+    #making samples
+    a_file = open("txt.json", "r")
+    a_json = json.load(a_file)
+    j = 1
+    for i in a_json['tests']:
+        os.chdir(f"{dir}/samples")
+        in_file = open(f"sample{j}.in","w")
+        out_file = open(f"sample{j}.out","w")
+        in_file.write(i['input'])
+        out_file.write(i['output'])
+        j += 1
+        os.chdir("/Users/sandisk/Desktop/tmp/programming/cpp2")
+    os.chdir(f"{dir}/samples")
+    os.system(f"cp /Users/sandisk/Desktop/tmp/programming/cpp2/main.cpp {dir}")
+    #print(list)
 
-def create_files():
+def create_files(dir):
+    os.chdir(dir)
+    os.system(f"cp /Users/sandisk/Desktop/tmp/programming/cpp2/Makefile {dir}")
+    make_samples(dir)
+    #os.chdir("/Users/sandisk/Desktop/tmp/programming/cpp2")
+    os.chdir("/Users/sandisk")
+    #create cpp file and use "main.cpp" as template file
+    #print(os.getcwd())
+    
+def create_dir(arg_list,j):
+    cur = os.getcwd()
+    os.system(f"mkdir {cur}/{arg_list[j]}")
+    create_files(f"{cur}/{arg_list[j]}")
+        #print(f'dir {cur}/{arg_list[i]}')
 
-    return 0
-def download_problem_to_json():
+#def make_prob(prob_count):
+#    arg_list = sys.argv
+#    create_dir(arg_list)
+
+def download_problem_to_json(j):
+    arg_list = sys.argv
     argument = '...'
     cur = os.getcwd()
-    os.chdir("/Users/sandisk/Desktop")
-    os.chdir("tmp")
-    os.chdir("programming")
-    os.chdir("cpp2")
+    os.chdir("/Users/sandisk/Desktop"),os.chdir("tmp"),os.chdir("programming"),os.chdir("cpp2")
     main_proc = subprocess.Popen(['node index.js -> txt.json', '', argument], shell=True)
     time.sleep(5)# <-- There's no time.wait, but time.sleep.
     if os.stat("txt.json").st_size != 0:
-        print("PROBLEM FOUND, KILLING PROCESS")
+        print("PROBLEM FOUND:")
         main_proc.terminate()
         proc1 = subprocess.Popen(['killall -9 node', '', argument], shell=True)
         pid = main_proc.pid # <--- access `pid` attribute to get the pid of the child process.
         pid1 = proc1.pid
         #for os proc double case so that it works without any exceptions/errors
         os.system("killall -9 node")
+        print_json()
         print('KILLED NODE PROCESSES')
         print()
-        print_json()
-        print()
-        #get_all()
+        os.chdir(cur)
+        create_dir(arg_list,j)
+        os.chdir("/Users/sandisk/Desktop"),os.chdir("tmp"),os.chdir("programming"),os.chdir("cpp2")
         #erase json
         open('txt.json', 'r+').truncate()
+        os.chdir(cur)
         #os.chdir("/Users/sandisk")
         #DEBUG: print("kill successful")
-        os.chdir(cur)
+        
 def main():
     prob_count = int(sys.argv[1])
     #print(prob_count)
-    arg_list = sys.argv
+    
     #print(arg_list)
+    #make_prob(prob_count)
+    j = 2
     for i in range(prob_count):
-       download_problem_to_json()
+       download_problem_to_json(j)
+       j += 1
     #print(arg_list[0])
 
 if __name__ == '__main__':
     main()
-#TODO: create folders,input/output files ("sample.in","sample.out" [make separate ones for each testcase given by problem]) ,reorganize makefiles,integrate using OOP?
+#TODO: reorganize and recreate makefile,integrate using OOP?,add helpbar if you are stuck on how to use, use try and except for download_json
---
+++ main.cpp
@@ -0,0 +1,97 @@
+//expansion of : #include <bits/stdc++.h> because of clang, use 2,76:fo
+#include <cassert>
+  #include <cctype>
+  #include <cerrno>
+  #include <cfloat>
+  #include <ciso646>
+  #include <climits>
+  #include <clocale>
+  #include <cmath>
+  #include <csetjmp>
+  #include <csignal>
+  #include <cstdarg>
+  #include <cstddef>
+  #include <cstdio>
+  #include <cstdlib>
+  #include <cstring>
+  #include <ctime>
+  #include <ccomplex>
+  #include <cfenv>
+  #include <cinttypes>
+  #include <cstdbool>
+  #include <cstdint>
+  #include <ctgmath>
+  #include <cwchar>
+  #include <cwctype>
+  #include <algorithm>
+  #include <bitset>
+  #include <complex>
+  #include <deque>
+  #include <exception>
+  #include <fstream>
+  #include <functional>
+  #include <iomanip>
+  #include <ios>
+  #include <iosfwd>
+  #include <iostream>
+  #include <istream>
+  #include <iterator>
+  #include <limits>
+  #include <list>
+  #include <locale>
+  #include <map>
+  #include <memory>
+  #include <new>
+  #include <numeric>
+  #include <ostream>
+  #include <queue>
+  #include <set>
+  #include <sstream>
+  #include <stack>
+  #include <stdexcept>
+  #include <streambuf>
+  #include <string>
+  #include <typeinfo>
+  #include <utility>
+  #include <valarray>
+  #include <vector>
+  #include <array>
+  #include <atomic>
+  #include <chrono>
+  #include <condition_variable>
+  #include <forward_list>
+  #include <future>
+  #include <initializer_list>
+  #include <mutex>
+  #include <random>
+  #include <ratio>
+  #include <regex>
+  #include <scoped_allocator>
+  #include <system_error>
+  #include <thread>
+  #include <tuple>
+  #include <typeindex>
+  #include <type_traits>
+  #include <unordered_map>
+  #include <unordered_set>
+using namespace std;
+#define endl "\n" 
+#define ll int64_t
+#define vt vector
+#define ar array
+#define sz(x) (int)(x.size())
+#define regsz(x) (int)(sizeof(x)/sizeof(x[0]))
+#define dbg(...) cout << "[" << __VA_ARGS__ << "]" << endl 
+const ll MOD = 1000000007;
+void io(string s){freopen((s+".in").c_str(),"r",stdin);freopen((s+".out").c_str(),"w",stdout);}
+int main(){
+    
+    return 0;
+}
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/          
